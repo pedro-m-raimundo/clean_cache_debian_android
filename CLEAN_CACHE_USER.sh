c66254c30ac2844a6612be0635e6e173ecb9fbc9
Pedro Raimundo <pedro.raimundo@ua.pt>
2018-02-18 23:12:58 +0000

    Updated to version 3.1

--- CLEAN_CACHE_USER.sh
+++ CLEAN_CACHE_USER.sh
@@ -5,9 +5,22 @@ source CLEAN_CACHE_SYSTEM.sh
 echo -e "\e[33m"
 
 # -------------------------------------------------------------------
-
-#Functions here
+#Functions to use below:
+#-> remove_folder
+#-> remove_file
+#-> remove_type_within_folder
+#-> remove_folder_within_folder
+#-> remove_file_within_folder
+#-> remove_folder_within_subfolder
+#-> remove_file_within_subfolder
+#-> remove_empty_files_folders
 
 # -------------------------------------------------------------------
+#Choose the functions here
 
+
+
+
+
+# -------------------------------------------------------------------
 echo -e "\e[0m"
