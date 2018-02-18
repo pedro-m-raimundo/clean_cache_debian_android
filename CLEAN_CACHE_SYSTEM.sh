e321d1639d77bcaf4ed5aeeb4d5d66704ce89c17
Pedro Raimundo <pedro.raimundo@ua.pt>
2018-02-18 23:34:15 +0000

    Updated to version 3.1.1

--- CLEAN_CACHE_SYSTEM.sh
+++ CLEAN_CACHE_SYSTEM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#version: 3.1
+#version: 3.1.1
 # -------------------------------------------------------------------
 function remove_folder {
 
@@ -39,288 +39,304 @@ function remove_file {
 # -------------------------------------------------------------------
 function remove_type_within_folder {
 
-	for j in "${@}" ; do
+	if [ -e "$1" ] ; then
 
-		if [ "${j}" = "$1" ] ; then
-				continue
+		for j in "${@}" ; do
 
-		else
-			arr1="$( ls -a "$1"/*".${j}" )"
-			
-			SAVEIFS=$IFS
-			IFS=$'\n'
-			array=($arr1)
-			IFS=$SAVEIFS
+			if [ "${j}" = "$1" ] ; then
+					continue
 
-			if [ "${#array[@]}" = 0 ] ; then
-				return
-			fi
+			else
+				arr1="$( ls -a "$1"/*".${j}" )"
+				
+				SAVEIFS=$IFS
+				IFS=$'\n'
+				array=($arr1)
+				IFS=$SAVEIFS
 
-			for i in "${array[@]}" ; do
-
-				if ! [ -d "${i}" ] ; then
-					rm "${i}"
-					OK="$?"
-
-					if [ "$OK" -eq 0 ] ; then
-						echo -e "\e[96m>> Deleted ${i}\e[33m"
-					fi
+				if [ "${#array[@]}" = 0 ] ; then
+					return
 				fi
-			done
-		fi
-		
-	done
+
+				for i in "${array[@]}" ; do
+
+					if ! [ -d "${i}" ] ; then
+						rm "${i}"
+						OK="$?"
+
+						if [ "$OK" -eq 0 ] ; then
+							echo -e "\e[96m>> Deleted ${i}\e[33m"
+						fi
+					fi
+				done
+			fi
+		done
+	fi
 }
 # -------------------------------------------------------------------
 function remove_folder_within_folder {
 
-	arr1="$( ls -a "$1")"
-	SAVEIFS=$IFS
-	IFS=$'\n'
-	array=($arr1)
-	IFS=$SAVEIFS
+	if [ -e "$1" ] ; then
 
-	if [ "${#array[@]}" = 2 ] ; then
-		return
-	fi
+		arr1="$( ls -a "$1")"
+		SAVEIFS=$IFS
+		IFS=$'\n'
+		array=($arr1)
+		IFS=$SAVEIFS
 
-	for i in "${array[@]}" ; do
-		for j in "${@}" ; do
+		if [ "${#array[@]}" = 2 ] ; then
+			return
+		fi
 
-			if [ "${j}" = "$1" ] ; then
-				continue
+		for i in "${array[@]}" ; do
+			for j in "${@}" ; do
 
-			else
-				if [ "${i}" = "." ] || [ "${i}" = ".." ] ; then
+				if [ "${j}" = "$1" ] ; then
 					continue
 
-				elif [ -d "$1/${i}" ] && [ "${i}" = "${j}" ] ; then
-					rm -r "$1/${i}"
-					OK="$?"
+				else
+					if [ "${i}" = "." ] || [ "${i}" = ".." ] ; then
+						continue
 
-					if [ "$OK" -eq 0 ] ; then
-						echo -e "\e[92m>> Deleted $1/${i}\e[33m"
-					fi		
+					elif [ -d "$1/${i}" ] && [ "${i}" = "${j}" ] ; then
+						rm -r "$1/${i}"
+						OK="$?"
+
+						if [ "$OK" -eq 0 ] ; then
+							echo -e "\e[92m>> Deleted $1/${i}\e[33m"
+						fi		
+					fi
 				fi
-			fi
+			done
 		done
-	done
+	fi
 }
 # -------------------------------------------------------------------
 function remove_file_within_folder {
 
-	arr1="$( ls -a "$1")"
-	SAVEIFS=$IFS
-	IFS=$'\n'
-	array=($arr1)
-	IFS=$SAVEIFS
+	if [ -e "$1" ] ; then
 
-	if [ "${#array[@]}" = 2 ] ; then
-		return
-	fi
+		arr1="$( ls -a "$1")"
+		SAVEIFS=$IFS
+		IFS=$'\n'
+		array=($arr1)
+		IFS=$SAVEIFS
 
-	for i in "${array[@]}" ; do
-		for j in "${@}" ; do
+		if [ "${#array[@]}" = 2 ] ; then
+			return
+		fi
 
-			if [ "${j}" = "$1" ] ; then
-				continue
+		for i in "${array[@]}" ; do
+			for j in "${@}" ; do
 
-			else
-				if [ "${i}" = "." ] || [ "${i}" = ".." ] ; then
+				if [ "${j}" = "$1" ] ; then
 					continue
-				elif ! [ -d "$1/${i}" ] && [ "${i}" = "${j}" ] ; then
-					rm "$1/${i}"
-					OK="$?"
 
-					if [ "$OK" -eq 0 ] ; then
-						echo -e "\e[96m>> Deleted $1/${i}\e[33m"
+				else
+					if [ "${i}" = "." ] || [ "${i}" = ".." ] ; then
+						continue
+					elif ! [ -d "$1/${i}" ] && [ "${i}" = "${j}" ] ; then
+						rm "$1/${i}"
+						OK="$?"
+
+						if [ "$OK" -eq 0 ] ; then
+							echo -e "\e[96m>> Deleted $1/${i}\e[33m"
+						fi
 					fi
 				fi
-			fi
+			done
 		done
-	done
+	fi
 }
 # -------------------------------------------------------------------
 function remove_folder_within_subfolder {
 
-	first_argument=$1
-	second_argument=$2
-	shift
-	shift
+	if [ -e "$1" ] ; then
 
-	if [ $second_argument = 0 ] ; then
-		return
-	fi
+		first_argument=$1
+		second_argument=$2
+		shift
+		shift
 
-	arr1="$( ls -a "$first_argument")"
-	SAVEIFS=$IFS
-	IFS=$'\n'
-	array=($arr1)
-	IFS=$SAVEIFS
+		if [ $second_argument = 0 ] ; then
+			return
+		fi
 
-	if [ "${#array[@]}" = 2 ] ; then
-		return
-	fi
+		arr1="$( ls -a "$first_argument")"
+		SAVEIFS=$IFS
+		IFS=$'\n'
+		array=($arr1)
+		IFS=$SAVEIFS
 
-	for i in "${array[@]}" ; do
-		for j in "${@}" ; do
+		if [ "${#array[@]}" = 2 ] ; then
+			return
+		fi
 
-			if [ "${j}" = "$first_argument" ] || [ "${j}" = "$second_argument" ] ; then
-				continue
+		for i in "${array[@]}" ; do
+			for j in "${@}" ; do
 
-			else
-				if [ "${i}" = "." ] || [ "${i}" = ".." ] ; then
+				if [ "${j}" = "$first_argument" ] || [ "${j}" = "$second_argument" ] ; then
 					continue
 
-				elif [ -d "$first_argument/${i}" ] && [ "${i}" = "${j}" ] ; then
-					rm -r "$first_argument/${i}"
-					OK="$?"
+				else
+					if [ "${i}" = "." ] || [ "${i}" = ".." ] ; then
+						continue
 
-					if [ "$OK" -eq 0 ] ; then
-						echo -e "\e[92m>> Deleted $first_argument/${i}\e[33m"
+					elif [ -d "$first_argument/${i}" ] && [ "${i}" = "${j}" ] ; then
+						rm -r "$first_argument/${i}"
+						OK="$?"
+
+						if [ "$OK" -eq 0 ] ; then
+							echo -e "\e[92m>> Deleted $first_argument/${i}\e[33m"
+						fi
+
+					elif [ -d "$first_argument/${i}" ] ; then
+						remove_folder_within_subfolder "$first_argument/${i}" $(($second_argument-1)) "${@}"
+						first_argument=${first_argument%/*}
+						second_argument=$(($second_argument+1))
 					fi
-
-				elif [ -d "$first_argument/${i}" ] ; then
-					remove_folder_within_subfolder "$first_argument/${i}" $(($second_argument-1)) "${@}"
-					first_argument=${first_argument%/*}
-					second_argument=$(($second_argument+1))
 				fi
-			fi
+			done
 		done
-	done
+	fi
 }
 # -------------------------------------------------------------------
 function remove_file_within_subfolder {
 
-	first_argument=$1
-	second_argument=$2
-	shift
-	shift
+	if [ -e "$1" ] ; then
 
-	if [ $second_argument = 0 ] ; then
-		return
-	fi
+		first_argument=$1
+		second_argument=$2
+		shift
+		shift
 
-	arr1="$( ls -a "$first_argument")"
-	SAVEIFS=$IFS
-	IFS=$'\n'
-	array=($arr1)
-	IFS=$SAVEIFS
+		if [ $second_argument = 0 ] ; then
+			return
+		fi
 
-	if [ "${#array[@]}" = 2 ] ; then
-		return
-	fi
+		arr1="$( ls -a "$first_argument")"
+		SAVEIFS=$IFS
+		IFS=$'\n'
+		array=($arr1)
+		IFS=$SAVEIFS
 
-	for i in "${array[@]}" ; do
-		for j in "${@}" ; do
+		if [ "${#array[@]}" = 2 ] ; then
+			return
+		fi
 
-			if [ "${j}" = "$first_argument" ] || [ "${j}" = "$second_argument" ] ; then
-				continue
+		for i in "${array[@]}" ; do
+			for j in "${@}" ; do
 
-			else
-				if [ "${i}" = "." ] || [ "${i}" = ".." ] ; then
+				if [ "${j}" = "$first_argument" ] || [ "${j}" = "$second_argument" ] ; then
 					continue
-				elif ! [ -d "$first_argument/${i}" ] && [ "${i}" = "${j}" ] ; then
-					rm "$first_argument/${i}"
-					OK="$?"
 
-					if [ "$OK" -eq 0 ] ; then
-						echo -e "\e[96m>> Deleted $first_argument/${i}\e[33m"
+				else
+					if [ "${i}" = "." ] || [ "${i}" = ".." ] ; then
+						continue
+					elif ! [ -d "$first_argument/${i}" ] && [ "${i}" = "${j}" ] ; then
+						rm "$first_argument/${i}"
+						OK="$?"
+
+						if [ "$OK" -eq 0 ] ; then
+							echo -e "\e[96m>> Deleted $first_argument/${i}\e[33m"
+						fi
+
+					elif [ -d "$first_argument/${i}" ] ; then
+						remove_file_within_subfolder "$first_argument/${i}" $(($second_argument-1)) "${@}"
+						first_argument=${first_argument%/*}
+						second_argument=$(($second_argument+1))
+							
 					fi
-
-				elif [ -d "$first_argument/${i}" ] ; then
-					remove_file_within_subfolder "$first_argument/${i}" $(($second_argument-1)) "${@}"
-					first_argument=${first_argument%/*}
-					second_argument=$(($second_argument+1))
-						
 				fi
-			fi
+			done
 		done
-	done
+	fi
 }
 # -------------------------------------------------------------------
 function remove_empty_files_folders {
 
-	first_argument=$1
-	second_argument=$2
-	shift
-	shift
+	if [ -e "$1" ] ; then
 
-	if [ $second_argument = 0 ] ; then
-		return
-	fi
+		first_argument=$1
+		second_argument=$2
+		shift
+		shift
 
-	arr1="$( ls -a "$first_argument")"
-	SAVEIFS=$IFS
-	IFS=$'\n'
-	array=($arr1)
-	IFS=$SAVEIFS
-
-	if [ "${#array[@]}" = 2 ] ; then
-		rm -r "$first_argument"
-		OK="$?"
-
-		if [ "$OK" -eq 0 ] ; then
-			echo -e "\e[92m>> Deleted $first_argument\e[33m"
+		if [ $second_argument = 0 ] ; then
+			return
 		fi
 
-		return
-	fi
+		arr1="$( ls -a "$first_argument")"
+		SAVEIFS=$IFS
+		IFS=$'\n'
+		array=($arr1)
+		IFS=$SAVEIFS
 
-	for i in "${array[@]}" ; do
-		NUMBER="${#@}"
-		for j in "${@}" ; do
-
-			if [ "${j}" = "$first_argument" ] || [ "${j}" = "$second_argument" ] ; then
-				continue
-
-			else
-				if [ "$first_argument/${i}" == "${j}" ] ; then
-					break
-
-				
-				fi
-			fi
-			NUMBER=$(($NUMBER-1))
-		done
-
-		if [ "${i}" = "." ] || [ "${i}" = ".." ] ; then
-			continue
-
-		elif ! [ -d "$first_argument/${i}" ] && ! [ -s "$first_argument/${i}" ] ; then
-			rm "$first_argument/${i}"
+		if [ "${#array[@]}" = 2 ] ; then
+			rm -r "$first_argument"
 			OK="$?"
 
 			if [ "$OK" -eq 0 ] ; then
-				echo -e "\e[96m>> Deleted $first_argument/${i}\e[33m"
+				echo -e "\e[92m>> Deleted $first_argument\e[33m"
 			fi
+
+			return
 		fi
 
-		if [ $NUMBER -eq 0 ] ; then
+		for i in "${array[@]}" ; do
+			NUMBER="${#@}"
+			for j in "${@}" ; do
 
-			if [ -d "$first_argument/${i}" ] ; then
+				if [ "${j}" = "$first_argument" ] || [ "${j}" = "$second_argument" ] ; then
+					continue
 
-				remove_empty_files_folders "$first_argument/${i}" $(($second_argument-1)) "${@}"
-				first_argument=${first_argument%/*}
-				second_argument=$(($second_argument+1))
+				else
+					if [ "$first_argument/${i}" == "${j}" ] ; then
+						break
+					fi
+				fi
+
+				NUMBER=$(($NUMBER-1))
+			done
+
+			if [ "${i}" = "." ] || [ "${i}" = ".." ] ; then
+				continue
+
+			elif ! [ -d "$first_argument/${i}" ] && ! [ -s "$first_argument/${i}" ] ; then
+				rm "$first_argument/${i}"
+				OK="$?"
+
+				if [ "$OK" -eq 0 ] ; then
+					echo -e "\e[96m>> Deleted $first_argument/${i}\e[33m"
+				fi
 			fi
+
+			if [ $NUMBER -eq 0 ] ; then
+
+				if [ -d "$first_argument/${i}" ] ; then
+
+					remove_empty_files_folders "$first_argument/${i}" $(($second_argument-1)) "${@}"
+					first_argument=${first_argument%/*}
+					second_argument=$(($second_argument+1))
+				fi
+			fi
+		done
+
+		arr1="$( ls -a "$first_argument")"
+		SAVEIFS=$IFS
+		IFS=$'\n'
+		array=($arr1)
+		IFS=$SAVEIFS
+
+		if [ "${#array[@]}" = 2 ] ; then
+			rm -r "$first_argument"
+			OK="$?"
+
+			if [ "$OK" -eq 0 ] ; then
+				echo -e "\e[92m>> Deleted $first_argument\e[33m"
+			fi
+
+			return
 		fi
-	done
-
-	arr1="$( ls -a "$first_argument")"
-	SAVEIFS=$IFS
-	IFS=$'\n'
-	array=($arr1)
-	IFS=$SAVEIFS
-
-	if [ "${#array[@]}" = 2 ] ; then
-		rm -r "$first_argument"
-		OK="$?"
-
-		if [ "$OK" -eq 0 ] ; then
-			echo -e "\e[92m>> Deleted $first_argument\e[33m"
-		fi
-
-		return
 	fi
 }
--- README.md
+++ README.md
@@ -100,6 +100,7 @@ In order for this to work on **Ubuntu**, you need:
 
 | Version number | Description |
 |----------------|:------------|
+| 3.1.1 | Added a condition to all functions (except "_remove_file_" and "_remove_folder_") to skip them if the main folder doesn't exist. |
 | 3.1 | Restored two functions where a folders and files can be directly deleted instead of searching for them ("_remove_file_" and "_remove_folder_"). |
 | 3.0 | Complete overhaul on the system functions. Many bugs led to the rewrite of all them. All of them were fixed, some of them were renamed and others were created. It was also added compatibility between Android and Ubuntu (they both use Shell). Performance is lower than the other versions, but reliability was increased. |
 | 2.3.2 | Fixed a bug on the last function, where at folders were not skipped. |
